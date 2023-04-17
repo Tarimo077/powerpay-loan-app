1a1ff710c10a8436f0ef142c66e24f7397fba640
givelimited@gmail.com <givelimited@gmail.com>
2023-04-17 16:56:27 +0000

    Edited theme.css, Chat and theme/parameters.yaml

--- client_code/Chat/__init__.py
+++ client_code/Chat/__init__.py
@@ -20,20 +20,24 @@ class Chat(ChatTemplate):
     prompt = self.text_box_1.text
     self.text_box_1.text = ""
     self.item = False
-    m = self.makeLabels(prompt)
+    m, y = self.makeLabels(prompt)
     self.linear_panel_1.add_component(m)
+    self.linear_panel_1.add_component(y)
     response = anvil.server.call('getChat', prompt)
     self.item = True
-    n = self.makeLabels(response)
+    n, b = self.makeLabels(response)
     self.linear_panel_1.add_component(n)
+    self.linear_panel_1.add_component(b)
 
   def makeLabels(self, prompt, **event_args):
     if(self.item==False):
-      c = Label(align="right", text=prompt, spacing_above="medium")
+      s = Label(align="right", text="You:\n", spacing_above="medium", icon="fa:comments", spacing_below="small", role="usr")
+      c = Label(align="right", text=prompt+" ", spacing_above="small", font_size=15, border=" 3px solid #8fce00")
     else:
       text = prompt.get_bytes().decode('utf-8')
-      c = Label(align="left", text=text, spacing_above="medium", bold=True, icon="fa:magic")
-    return c
+      s = Label(align="left", text="PPBot:\n", spacing_above="medium", icon="fa:magic", spacing_below="small", role="bot")
+      c = Label(align="left", text=" "+text, spacing_above="small", font_size=15, border=" 3px solid #ffa500")
+    return s,c
 
   def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
