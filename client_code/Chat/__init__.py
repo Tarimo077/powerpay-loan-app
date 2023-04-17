105d4ab6f925464b14a98000ea96a197d6bf1454
givelimited@gmail.com <givelimited@gmail.com>
2023-04-17 11:53:21 +0000

    Edited Chat

--- client_code/Chat/__init__.py
+++ client_code/Chat/__init__.py
@@ -10,7 +10,6 @@ class Chat(ChatTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.
 
     # Any code you write here will run before the form opens.
 
