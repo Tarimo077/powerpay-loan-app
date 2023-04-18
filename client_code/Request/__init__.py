98810e9ce83e365eb2095db36aab4fcfad32c262
givelimited@gmail.com <givelimited@gmail.com>
2023-04-18 10:42:17 +0000

    Edited Request

--- client_code/Request/__init__.py
+++ client_code/Request/__init__.py
@@ -20,6 +20,16 @@ class Request(RequestTemplate):
     my_array = json.loads(text)
     sorted_items = sorted(my_array, key=lambda x: x['deviceID'])
     self.repeating_panel_1.items = sorted_items
+    active_devs = 0
+    inactive_devs = 0
+    for obj in my_array:
+      if obj["active"]:
+        active_devs += 1
+      else:
+        inactive_devs += 1
+
+    self.activeDevs.text = str(active_devs)
+    self.inactiveDevs.text = str(inactive_devs)
     #my_obj = ItemTemplate6()
     #usr = self.item
     #my_obj.getItem(usr)
