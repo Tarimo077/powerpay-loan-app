9f9fa089d5a952402a68ccdfd8642fcec0b991b1
givelimited@gmail.com <givelimited@gmail.com>
2023-08-03 09:47:01 +0000

    Created Settings; Edited settings

---
+++ client_code/Settings/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import SettingsTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Settings(SettingsTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
