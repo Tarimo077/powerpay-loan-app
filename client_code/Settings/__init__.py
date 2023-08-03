bba89d10271bf44a08e589e80290dfc03abf153d
givelimited@gmail.com <givelimited@gmail.com>
2023-08-03 13:01:20 +0000

    Edited settings and Settings

--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -12,5 +12,16 @@ class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    usr = anvil.server.call('getusername')
+    words = usr.split()
+# Extract the first character of each word and convert it to uppercase
+    initials = [word[0].upper() for word in words]
+# Join the initials together
+    initials_string = ''.join(initials)
+    self.username_label.text = initials_string
     # Any code you write here will run before the form opens.
+    d = app_tables.users.get(username=usr)
+    self.f_name.text = words[0]
+    self.s_name.text = words[1]
+    self.email.text = d['email']
+    self.phone.text = d['phone']
