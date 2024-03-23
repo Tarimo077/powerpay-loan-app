fb88b57fa8f3c85596074047b0f59898b7f045d4
givelimited@gmail.com <givelimited@gmail.com>
2024-03-23 15:55:05 +0000

    Created Try

---
+++ client_code/Try/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import TryTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Try(TryTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
