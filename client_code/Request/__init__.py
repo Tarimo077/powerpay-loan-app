0dafcbd5aec5e2c83c441f8330d8167e2d57d762
givelimited@gmail.com <givelimited@gmail.com>
2024-03-26 06:17:42 +0000

    Edited Request and settings

--- client_code/Request/__init__.py
+++ client_code/Request/__init__.py
@@ -28,7 +28,9 @@ class Request(RequestTemplate):
     response = anvil.server.call('req', dt)
     text = response.get_bytes().decode('utf-8')
     my_array = json.loads(text)
-    sorted_items = sorted(my_array, key=lambda x: x['deviceID'])
+    print(my_array)
+    sorted_items = sorted(my_array, key=lambda x: int(x['deviceID'].split('device')[-1]))
+    #sorted_items = sorted(my_array, key=lambda x: x['deviceID'])
     self.repeating_panel_1.items = sorted_items
     anvil.server.call('strDevArr', sorted_items)
     self.item = sorted_items
