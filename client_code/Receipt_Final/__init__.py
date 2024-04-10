2ca0cff18f235f99f92df72cd5957ade3dcc3998
givelimited@gmail.com <givelimited@gmail.com>
2024-04-10 13:13:05 +0000

    Created Receipt_Final; Edited Receipt and Receipt.RowTemplate10

---
+++ client_code/Receipt_Final/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import Receipt_FinalTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Receipt_Final(Receipt_FinalTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    media_object = anvil.server.call('create_pdf')
+    anvil.media.download(media_object)
